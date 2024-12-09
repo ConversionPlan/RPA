ec9380baa7d5fe08af6c88225980cc86ebb52165
Violeta <violeta@tracktracerx.com>
2024-12-09 15:18:13 -0300

    Add Aggreggation Product

--- features/steps/auth.py
+++ features/steps/auth.py
@@ -6,39 +6,107 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
+from datetime import datetime
+import json
+
+
+@given("User exists")
+def starts_timer(context):
+    context.initial_time = datetime.now().strftime("%H:%M:%S")
+
+
+@then("End test")
+def ends_timer(context):
+    context.final_time = datetime.now().strftime("%H:%M:%S")
+    with open("report/test_times.json", "r") as file:
+        time_json = json.load(file)
+
+    time_json.append({"start": context.initial_time, "end": context.final_time})
+    with open("report/test_times.json", "w") as file:
+        file.write(json.dumps(time_json))
+
+    context.driver.close()
+
+@given("Is Logged In")
+def is_logged_in(context):
+    try:
+        launchBrowser(context)
+        context.driver.implicitly_wait(5)
+        openLoginURL(context, "https://demopharmacoltd.qa-test.tracktraceweb.com/auth")
+        enterEmail(context, "rpa-user@tracktracerx.com")
+        clickNextToLogin(context)
+        enterPassword(context, "Rpa!1234")
+        clickSubmitButton(context)
+    except:
+        ends_timer(context)
+        raise
 
 @given("Launching Chrome browser")
 def launchBrowser(context):
-    options = Options()
-    options.add_argument("--headless=new")
-    context.driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options)
+    try:
+        options = Options()
+        # options.add_argument("--headless=new")
+        context.driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),
+            options=options)
+    except:
+        ends_timer(context)
+        raise
+
 
 @when('Open Portal Login page {url}')
 def openLoginURL(context, url):
-    context.driver.maximize_window()
-    context.driver.get(url)
+    try:
+        context.driver.maximize_window()
+        context.driver.get(url)
+    except:
+        ends_timer(context)
+        raise
+
 
 @when('Enter Username {email}')
 def enterEmail(context, email):
-    context.driver.find_element(by=By.ID, value="auth__login_form__username").send_keys(email)
+    try:
+        context.driver.find_element(by=By.ID, value="auth__login_form__username").send_keys(email)
+    except:
+        ends_timer(context)
+        raise
+
 
 @when('Click Next to Login')
 def clickNextToLogin(context):
-    context.driver.find_element(by=By.ID, value="auth__login_form__step1_next_btn").click()
+    try:
+        context.driver.find_element(by=By.ID, value="auth__login_form__step1_next_btn").click()
+    except:
+        ends_timer(context)
+        raise
+
 
 @when('Enter Password {password}')
 def enterPassword(context, password):
-    password_input = context.driver.find_element(by=By.ID, value="auth__login_form__password")
-    wait = WebDriverWait(context.driver, timeout=3)
-    wait.until(lambda d : password_input.is_displayed())
-    password_input.send_keys(password)
+    try:
+        password_input = context.driver.find_element(by=By.ID, value="auth__login_form__password")
+        wait = WebDriverWait(context.driver, timeout=3)
+        wait.until(lambda d: password_input.is_displayed())
+        password_input.send_keys(password)
+    except:
+        ends_timer(context)
+        raise
+
 
 @when('click on the Login button')
 def clickSubmitButton(context):
-    context.driver.find_element(by=By.ID, value="auth__login_form__step2_next_btn").click()
+    try:
+        context.driver.find_element(by=By.ID, value="auth__login_form__step2_next_btn").click()
+    except:
+        ends_timer(context)
+        raise
+
 
 @then('User must login successfully')
 def assertLogin(context):
-    context.driver.implicitly_wait(5)
-    context.driver.find_element(by=By.ID, value="tt2_ui__header__main_container")
-    context.driver.close()
+    try:
+        WebDriverWait(context.driver, 10).until(lambda x: x.find_element(by=By.CLASS_NAME, value="dashboard_icon"))
+    except:
+        ends_timer(context)
+        raise
