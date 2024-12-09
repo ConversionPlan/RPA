e90342c20060747fd1da69d83e6c6fea3188c258
Violeta <violeta@tracktracerx.com>
2024-12-09 11:49:44 -0300

    Add more steps to product creation

--- features/02-product.feature
+++ features/02-product.feature
@@ -1,7 +1,7 @@
 @Product_Management
 Feature: Product Management
 
-  Scenario: Create a Product
+  Scenario: Create an Each Product
     Given User exists
     And Is Logged In
     When Open dashboard page
@@ -10,12 +10,19 @@ Feature: Product Management
     And Click on Add - Product Management Page
     And Add Product Name
     And Click on Identifiers tab
+    And Add SKU
+    And Add UPC
     And Add GS1 Company Prefix
     And Add GS1 ID
     And Click on Add Identifier
     And Wait for Identifier Options to Load
     And Click on Identifier Value
     And Click on Add NDC
+    And Click on Requirements Tab
+    And Add Generic Name
+    And Add Strength
+    And Add Net Content Description
+    And Add Notes
     And Click on Add
     Then Product should be saved
     And End test
--- features/steps/product.py
+++ features/steps/product.py
@@ -1,6 +1,6 @@
 from behave import *
 from selenium.webdriver.common.by import By
-from features.steps.utils import generate_ndc, generate_gs1_id, generate_company_prefix, generate_product_name
+from features.steps.utils import generate_ndc, generate_gs1_id, generate_company_prefix, generate_product_name, generate_gtin_with_cp_id, generate_text_with_n_chars
 
 @when("Open dashboard page")
 def open_dashboard_page(context):
@@ -20,23 +20,33 @@ def click_add_product_management(context):
 
 @when("Add Product Name")
 def input_product_name(context):
-    product_name = generate_product_name()
-    context.product_name = product_name
-    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__1_name").send_keys(product_name)
+    context.product_name = generate_product_name()
+    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__1_name").send_keys(context.product_name)
 
 @when("Click on Identifiers tab")
 def click_identifiers_tab(context):
     context.driver.find_element(by=By.XPATH, value="//li[@rel='identifiers']").click()
 
+@when("Add SKU")
+def add_sku(context):
+    context.ndc = generate_ndc()
+
+    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__1_sku").send_keys(context.ndc)
+
+@when("Add UPC")
+def add_upc(context):
+    context.company_prefix = generate_company_prefix()
+    context.gs1_id = generate_gs1_id()
+    context.gtin = generate_gtin_with_cp_id(context.company_prefix, context.gs1_id)
+    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__1_upc").send_keys(context.gtin)
+
 @when("Add GS1 Company Prefix")
 def input_gs1_company_prefix(context):
-    company_prefix = generate_company_prefix()
-    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__1_gs1_company_prefix").send_keys(company_prefix)
+    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__1_gs1_company_prefix").send_keys(context.company_prefix)
 
 @when("Add GS1 ID")
 def input_gs1_id(context):
-    gs1_id = generate_gs1_id()
-    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__1_gs1_id").send_keys(gs1_id)
+    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__1_gs1_id").send_keys(context.gs1_id)
 
 @when("Click on Add Identifier")
 def click_add_identifier(context):
@@ -44,8 +54,7 @@ def click_add_identifier(context):
 
 @when("Click on Identifier Value")
 def input_ndc_value(context):
-    ndc = generate_ndc()
-    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__2_value").send_keys(ndc)
+    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__2_value").send_keys(context.ndc)
 
 @when("Wait for Identifier Options to Load")
 def wait_identifier_options(context):
@@ -54,6 +63,27 @@ def wait_identifier_options(context):
 @when("Click on Add NDC")
 def click_add_ndc(context):
     context.driver.find_element(by=By.XPATH, value="//div[contains(@class, 'tt_utils_ui_dlg_modal-width-class-l')]//button[contains(@class, 'tt_utils_ui_dlg_modal-default-enabled-button')]").click()
+
+@when("Click on Requirements Tab")
+def click_requirements_tab(context):
+    context.driver.find_element(by=By.XPATH, value="//li[@rel='requirements']").click()
+
+@when("Add Generic Name")
+def add_generic_name(context):
+    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__1_generic_name").send_keys(context.product_name)
+
+@when("Add Strength")
+def add_strength(context):
+    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__1_strength").send_keys("RPA Strength")
+
+@when("Add Net Content Description")
+def add_net_content(context):
+    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__1_net_content_desc").send_keys("RPA Net Content")
+
+@when("Add Notes")
+def add_notes(context):
+    text = generate_text_with_n_chars(30)
+    context.driver.find_element(by=By.ID, value="TT_UTILS_UI_FORM_UUID__1_notes").send_keys(text)
 
 @when("Click on Add")
 def click_add_ndc(context):
