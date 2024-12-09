3b6bfc8822d4deb1741939daecc1d6f956a5fa8b
Violeta <violeta@tracktracerx.com>
2024-12-09 11:19:20 -0300

    Add Create Product test

---
+++ README.md
@@ -0,0 +1,33 @@
+# RPA
+
+RPA for Track Trace RX's portal. It runs after every push to the `main` branch of this repository, and daily at 6am.
+
+All tests in order:
+1. Login Feature.
+   1. Login to Portal with valid parameters
+2. Product Feature.
+   1. Create a Product
+
+# Run Locally
+
+In order to run this repository locally, you need to have [Python](https://www.python.org/downloads/) installed.
+
+After having clone this repository, to install all the necessary packages, go to the root directory and run:
+
+```pip install -r requirements.txt```
+
+Then, to run all the tests at the same time:
+
+```behave --format json --outfile report/results.json```
+
+Or to run a specific test:
+
+```behave .\features\{SPECEFICI_TEST_PY}.feature --format json --outfile report/results.json```
+
+And replace {SPECIFIC_TEST_PY} by the name of the test you want to run.
+
+Then, to generate the PDF Report:
+
+```python .\reports\generate_pdf_py```
+
+The file will be found in `.\reports\Track_Validation.pdf`
--- report/generate_pdf.py
+++ report/generate_pdf.py
@@ -7,12 +7,10 @@ import json
 from datetime import datetime
 
 def generate_pdf():
-    print("PATH:", os.path.abspath(os.getcwd()))
-
     # Texts for the PDF
     portal: str = "QA Assurance"
     reference_report: str = "ReportRPA_01"
-    cur_date: str = datetime.now().strftime("%m/%d/%Y")
+    current_date: str = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
     author: str = "Violeta Carvalho"
     output_path: str = "report/Track_Validation.pdf"
 
@@ -24,7 +22,7 @@ def generate_pdf():
     big_font = 24
     left_margin = 50
 
-    # Create 01 Page Document
+    # Create First Document Page
     cnv = canvas.Canvas(output_path, pagesize=A4)
     pdfmetrics.registerFont(
         TTFont('Arial', 'report/arial.ttf'))  # Replace 'arial.ttf' with the actual path to your Arial font file
@@ -33,87 +31,110 @@ def generate_pdf():
     image_path = os.path.join("report", "Logo_TrackTraceRX.png")
     cnv.drawImage(image_path, 300, 690, width=250, height=40)
 
-    # Text = TrackRX Validation Report for
+    # Add Document Title
     cnv.setFont(font_name, big_font)
     cnv.drawString(left_margin, 550, "TrackRX Validation Report for")
     cnv.drawString(left_margin, 525, portal)
 
-    # Text = Reference Report
+    # Add Document Metadata
     cnv.setFont(font_name, medium_small_font)
-    cnv.drawString(left_margin, 350, "Report Reference Number:")
-    cnv.drawString(220, 350, reference_report)
+    cnv.drawString(left_margin, 350, f"Report Reference Number: {reference_report}")
+    cnv.drawString(left_margin, 330, f"Date of Issue: {current_date}")
+    cnv.drawString(left_margin, 310, f"Author: {author}")
 
-    # Text = Date Report
-    cnv.drawString(left_margin, 330, "Date of Issue:")
-    cnv.drawString(140, 330, cur_date)
-
-    # Text = Author RPA
-    cnv.drawString(left_margin, 310, "Author:")
-    cnv.drawString(100, 310, author)
-
-    # Save 01 Page Document
+    # Save First Document Page
     cnv.showPage()
 
-    # Text = Execultive Summary
+    # Add Description
     cnv.setFont(font_name, medium_font)
     cnv.drawString(left_margin, 750, "1. Executive Summary")
 
-    # Text = Text of Executive Summary
     cnv.setFont(font_name, small_font)
-    cnv.drawString(left_margin, 720, "This report details the validation activities performed from " + cur_date + " for the Customers")
+    cnv.drawString(left_margin, 720, "This report details the validation activities performed from " + current_date + " for the Customers")
     cnv.drawString(left_margin, 700, "Portal capturing findings from the RPA phases.")
-    cnv.drawString(left_margin, 680, "The TrackRX Portal successfully met all validation criteria.")
 
-    # Text = Introduction Summary
     cnv.setFont(font_name, medium_font)
     cnv.drawString(left_margin, 620, "2. Introduction")
 
-    # Text = Text of Introduction Summary
     cnv.setFont(font_name, small_font)
     cnv.drawString(left_margin, 590, "The validation activities are intended to confirm that the TrackRX Portal is installed, operates,")
     cnv.drawString(left_margin, 570, "and performs according to manufacturer specifications and company requirements.")
 
-    # Text = Deviations and Corrective Actions Summary
     cnv.setFont(font_name, medium_font)
     cnv.drawString(left_margin, 510, "3. Deviations and Corrective Actions")
 
-    # Text = Text of Deviations and Corrective Actions Summary
     cnv.setFont(font_name, small_font)
     cnv.drawString(left_margin, 480, "No deviations were observed during the validation process.")
 
-    # Text = Methodology Summary
     cnv.setFont(font_name, medium_font)
     cnv.drawString(left_margin, 420, "4. Methodology")
 
-    # Text = Text of Deviations and Corrective Actions Summary
     cnv.setFont(font_name, small_font)
     cnv.drawString(left_margin, 390, "You can find the entire detailed process that was performed by Track Trace RPA on the")
     cnv.drawString(left_margin, 370, "next page.")
 
-    # Save 02 Page Document
+    # Save Second Document Page
     cnv.showPage()
 
+    # Add Detailed Process Title
     cnv.setFont(font_name, medium_font)
-    cnv.drawString(left_margin, 750, "5. Detailed RPA Process")
+    y = 750
+    cnv.drawString(left_margin, y, "5. Detailed RPA Process")
 
 
+    # Get Test Data
     with open("report/results.json", "r") as file:
         results_json = json.load(file)
 
+    with open("report/test_times.json", "r") as file:
+        times_json = json.load(file)
+
+    index = 0
+    last_module = None
     for result in results_json:
-        y = 710
-        tag = result["tags"][0]
-        cnv.setFont(font_name, medium_small_font)
-        cnv.drawString(left_margin, y, f"Module: {tag}")
+        # Module
+        tag = result["tags"][0].replace("_", " ")
+
+        y -= 10
+
+        # If current module is different from last, add as header
+        if last_module != tag:
+            y -= 30
+            if y <= 70:
+                cnv.showPage()
+                y = 750
+
+            cnv.setFont(font_name, medium_small_font)
+            cnv.drawString(left_margin, y, f"Module: {tag}")
+
+        # If last module still not set, set it
+        if last_module is None:
+            last_module = tag
+
         cnv.setFont(font_name, small_font)
 
+        # Run through each test from a module
         for element in result["elements"]:
             y -= 20
+            if y <= 70:
+                cnv.showPage()
+                y = 750
+
             report = element["name"]
             result = element["status"].capitalize()
             cnv.drawString(left_margin, y, f"Report: {report} - {result}")
 
-        cnv.save()
+            y -= 20
+            time_json = times_json[index]
+            index += 1
+
+            cnv.drawString(left_margin, y, f"Start Time: {time_json["start"]} / End Time: {time_json["end"]}")
+
+    cnv.save()
+
+    # Reset Test Times json
+    with open("report/test_times.json", "w") as file:
+        file.write("[]")
 
 
 generate_pdf()
